11005385d4b417c4ef5cb8736b09a747d05e2e16
Kisanth <kisanth2004@gmail.com>
2026-06-05 15:48:39 +0530

    Update CORS

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
+// // // // const express = require("express");
+// // // // const mongoose = require("mongoose");
+// // // // const cors = require("cors");
+// // // // require("dotenv").config();
+
+// // // // const app = express();
+
+// // // // // Middleware
+// // // // app.use(cors());
+// // // // app.use(express.json());
+
+// // // // // MongoDB Connection
+// // // // mongoose
+// // // //   .connect(process.env.MONGO_URI)
+// // // //   .then(() => console.log("MongoDB Connected 🚀"))
+// // // //   .catch((err) => console.log(err));
+
+// // // // // Test Route
+// // // // app.get("/", (req, res) => {
+// // // //   res.send("KishCode Backend Running 🚀");
+// // // // });
+
+// // // // const PORT = process.env.PORT || 5000;
+
+// // // // app.listen(PORT, () => {
+// // // //   console.log(`Server running on port ${PORT}`);
+// // // // });
+
+
+
 // // // const express = require("express");
 // // // const mongoose = require("mongoose");
 // // // const cors = require("cors");
@@ -15,6 +45,9 @@
 // // //   .then(() => console.log("MongoDB Connected 🚀"))
 // // //   .catch((err) => console.log(err));
 
+// // // // Routes
+// // // app.use("/api/projects", require("./routes/projectRoutes"));
+
 // // // // Test Route
 // // // app.get("/", (req, res) => {
 // // //   res.send("KishCode Backend Running 🚀");
@@ -27,61 +60,53 @@
 // // // });
 
 
-
 // // const express = require("express");
 // // const mongoose = require("mongoose");
 // // const cors = require("cors");
 // // require("dotenv").config();
 
+// // const contactRoute = require("./routes/contact");
+
 // // const app = express();
 
-// // // Middleware
 // // app.use(cors());
 // // app.use(express.json());
 
-// // // MongoDB Connection
-// // mongoose
-// //   .connect(process.env.MONGO_URI)
-// //   .then(() => console.log("MongoDB Connected 🚀"))
+// // app.use("/api/contact", contactRoute);
+
+// // mongoose.connect(process.env.MONGO_URI)
+// //   .then(() => console.log("MongoDB Connected"))
 // //   .catch((err) => console.log(err));
 
-// // // Routes
-// // app.use("/api/projects", require("./routes/projectRoutes"));
-
-// // // Test Route
-// // app.get("/", (req, res) => {
-// //   res.send("KishCode Backend Running 🚀");
-// // });
-
-// // const PORT = process.env.PORT || 5000;
-
-// // app.listen(PORT, () => {
-// //   console.log(`Server running on port ${PORT}`);
+// // app.listen(process.env.PORT, () => {
+// //   console.log("Server running on port", process.env.PORT);
 // // });
 
 
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// require("dotenv").config();
 
-// const contactRoute = require("./routes/contact");
+
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import connectDB from "./config/db.js";
+// import contactRoutes from "./routes/contactRoutes.js";
+
+// dotenv.config();
+// connectDB();
 
 // const app = express();
 
 // app.use(cors());
 // app.use(express.json());
 
-// app.use("/api/contact", contactRoute);
+// // routes
+// app.use("/api/contact", contactRoutes);
 
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => console.log("MongoDB Connected"))
-//   .catch((err) => console.log(err));
+// const PORT = process.env.PORT || 5000;
 
-// app.listen(process.env.PORT, () => {
-//   console.log("Server running on port", process.env.PORT);
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
 // });
-
 
 
 
@@ -92,16 +117,34 @@ import connectDB from "./config/db.js";
 import contactRoutes from "./routes/contactRoutes.js";
 
 dotenv.config();
+
+// Connect MongoDB
 connectDB();
 
 const app = express();
 
-app.use(cors());
+// Middleware
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://genuine-kitten-4514af.netlify.app",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-// routes
+// Test Route
+app.get("/", (req, res) => {
+  res.send("KishCode Backend Running 🚀");
+});
+
+// Contact Route
 app.use("/api/contact", contactRoutes);
 
+// Port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
